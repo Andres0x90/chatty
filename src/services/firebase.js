b1c4280d58941f4e7434818c7f72e11308edaa62
Andres Cano <andrescaza246@hotmail.com>
2021-10-17 02:04:44 -0500

    Chat terminado con autenticacion y conectado a la db de firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,11 +1,17 @@
-import firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+import {getDatabase} from  'firebase/database';
 
 const config = {
-    apiKey: "ADD-YOUR-DETAILS-HERE",
-    authDomain: "ADD-YOUR-DETAILS-HERE",
-    databaseURL: "ADD-YOUR-DETAILS-HERE"
-  };
+  apiKey: "AIzaSyAoFn-ix8eTFTlbTfJX6zLmDbA_deQ9Li0",
+  authDomain: "question-and-answer-df3c7.firebaseapp.com",
+  projectId: "question-and-answer-df3c7",
+  storageBucket: "question-and-answer-df3c7.appspot.com",
+  messagingSenderId: "816410372904",
+  appId: "1:816410372904:web:d5a93c84da39fa2d1f7e1d",
+  databaseURL: "https://question-and-answer-df3c7-default-rtdb.firebaseio.com/" 
+ };
 
 firebase.initializeApp(config);
-export const auth = firebase.auth();
-export const db = firebase.db();
+export const auth = getAuth();
+export const db = getDatabase();
